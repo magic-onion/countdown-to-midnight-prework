768ed33ad56f5a69e8a908bc4bace611c95f2986
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-25 01:26:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,13 @@ def countdown(counter)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(counter)
+  counter = 10
+  while counter > 0 
+   puts "#{counter} SECOND(S)!"
+    counter -= 1 
+    sleep(0.5)
+  end
+  "HAPPY NEW YEAR!"
+end
